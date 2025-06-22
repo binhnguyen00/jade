ede98d83857b41eda5f48964e930a35bc6a9d695
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-06-23 00:11:57 +0700

    remove puter apis

--- ui/src/hooks/index.ts
+++ ui/src/hooks/index.ts
@@ -1,15 +1,9 @@
 import { useScriptTag } from "./useScriptTag";
-import { useOpenGPT } from "./useOpenGPT";
-import { useDeepSeek } from "./useDeepSeek";
 import { useOpenRouter } from "./useOpenRouter";
-import { useChat } from "./useChat";
 import { useViteKey } from "./useViteKey";
 
 export { 
   useScriptTag,
-  useOpenGPT,
-  useDeepSeek,
-  useChat,
   useOpenRouter,
   useViteKey,
 }
--- ui/src/hooks/useOpenRouter.ts
+++ ui/src/hooks/useOpenRouter.ts
@@ -1,40 +1,43 @@
+import React from "react";
 import { useMutation } from "@tanstack/react-query";
 
 import { RequestMethod, OpenRouterFreeModel } from "types";
 import { useViteKey } from "./useViteKey";
 
 export function useOpenRouter() {
+  const OPENROUTER_API_KEY = useViteKey({ key: "VITE_OPEN_ROUTER_API_KEY" }) as string;
+
+  const mutationFn = async ({ prompt, model }: { prompt: string, model: OpenRouterFreeModel }) => {
+    const input = "https://openrouter.ai/api/v1/chat/completions";
+    const init: RequestInit = {
+      method: RequestMethod.POST,
+      headers: {
+        "Authorization": `Bearer ${OPENROUTER_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: model,
+        messages: [
+          { role: "user", content: prompt },
+        ],
+      }),
+    }
+    const response = await fetch(input, init);
+    if (!response.ok) {
+      throw new Error("useOpenRouter.ts: error fetching data");
+    }
+    return response.json();
+  }
+
   const { mutate, isPending, error, data } = useMutation({
     mutationKey: [ "openRouter" ],
     mutationFn: mutationFn,
     retry: false,
   })
 
-  return { trigger: mutate, isPending, error, data }
-}
+  const chat = React.useCallback((props: { prompt: string, model?: OpenRouterFreeModel }) => {
+    const { prompt, model = OpenRouterFreeModel.DEEPSEEK_R1 } = props;
+    mutate({ prompt, model });
+  }, [ mutate ])
 
-async function mutationFn({ prompt, model }: {
-  prompt: string,
-  model: OpenRouterFreeModel,
-}): Promise<any> {
-  const openRouterApiKey = useViteKey({ key: "VITE_OPEN_ROUTER_API_KEY" }) as string;
-  
-  const input = "https://openrouter.ai/api/v1/chat/completions";
-  const init: RequestInit = {
-    method: RequestMethod.POST,
-    headers: {
-      "Authorization": `Bearer ${openRouterApiKey}`,
-    },
-    body: JSON.stringify({
-      model: model,
-      messages: [
-        { role: "user", content: prompt },
-      ],
-    }),
-  }
-  const response = await fetch(input, init);
-  if (!response.ok) {
-    throw new Error("useOpenRouter.ts: error fetching data");
-  }
-  return response.json();
+  return { chat, isPending, error, data }
 }
