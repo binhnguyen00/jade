c9add1f7d1255176aed4eb1a8980754182ff09c7
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-06-27 14:53:19 +0700

    clean code

--- ui/src/types/index.ts
+++ ui/src/types/index.ts
@@ -3,11 +3,9 @@ import { RequestMethod } from "./server";
 import { OpenRouterFreeModel } from "./openRouter";
 
 import type { Conversation } from "./conversation";
-import type { PuterAPI, SupportModels } from "./puter";
 import type { SuccessCB, FailCB, ChatResponse } from "./server";
 
 export { 
-  SuccessCB, FailCB, ChatResponse, RequestMethod, 
-  OpenRouterFreeModel,
-  ChatModel, PuterAPI, SupportModels, Conversation, 
+  SuccessCB, FailCB, ChatResponse, RequestMethod,
+  OpenRouterFreeModel, ChatModel, Conversation,
 }
