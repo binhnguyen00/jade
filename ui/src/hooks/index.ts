571ab45062d66d7eda7599429f82aae7941b8fe5
Binh Nguyen <jackjack2000.kahp@gmail.com>
2025-07-21 15:09:35 +0700

    clean code

--- ui/src/hooks/index.ts
+++ ui/src/hooks/index.ts
@@ -1,9 +1,7 @@
-import { useScriptTag } from "./useScriptTag";
 import { useOpenRouter } from "./useOpenRouter";
 import { useViteKey } from "./useViteKey";
 
 export { 
-  useScriptTag,
   useOpenRouter,
   useViteKey,
 }
